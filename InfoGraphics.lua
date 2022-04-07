402a6764d735d71f8b71a85153a67a05dad9de30
TheNewTimeGamer <32019721+TheNewTimeGamer@users.noreply.github.com>
2022-04-07 08:40:54 +0200

    FrameStack.

--- InfoGraphics.lua
+++ InfoGraphics.lua
@@ -1,11 +1,34 @@
 if TNTG == nil then
     TNTG = {}
 end
-TNTG.nameplates = {}
-TNTG.last = 10
+TNTG.Frames = {}
+TNTG.Nameplates = {}
+TNTG.Last = 10
+
+PlayerFrame:Hide();
+TargetFrame:Hide();
+
+function CreateNamePlateFrame(ParentFrame, PreviousFrame, NamePlateUnitToken)
+    TNTG.Frames[NamePlateUnitToken] = CreateFrame("Frame", "TNTG-" .. NamePlateUnitToken, ParentFrame)
+    TNTG.Frames[NamePlateUnitToken]:SetSize(100, 20)
+    if PreviousFrame ~= nil then
+        TNTG.Frames[NamePlateUnitToken]:SetPoint("TOP", PreviousFrame, "BOTTOM", 0, 0)
+    else
+        TNTG.Frames[NamePlateUnitToken]:SetPoint("TOP", ParentFrame, "TOP", 0, 0)
+    end
+    TNTG.Frames[NamePlateUnitToken]:SetFrameStrata("BACKGROUND")
+
+    local defaultTexture = TNTG.Frames[NamePlateUnitToken]:CreateTexture(nil, "BACKGROUND")
+    defaultTexture:SetColorTexture(0,0,0,1)
+    defaultTexture:SetAllPoints(TNTG.Frames[NamePlateUnitToken])
+    TNTG.Frames[NamePlateUnitToken].texture = defaultTexture
+
+    TNTG.Frames[NamePlateUnitToken]:Show()
+    return TNTG.Frames[NamePlateUnitToken]
+end
 
 function UpdateNamePlate(nameplate)
-    TNTG.nameplates[nameplate.namePlateUnitToken] = {
+    TNTG.Nameplates[nameplate.namePlateUnitToken] = {
         Nameplate = nameplate,
         UnitName = UnitName(nameplate.namePlateUnitToken),
         UnitGUID = UnitGUID(nameplate.namePlateUnitToken),
@@ -31,10 +54,10 @@ function UpdateNamePlate(nameplate)
     }
 end
 
-local nameplates = C_NamePlate.GetNamePlates(false)
-for i, v in pairs(nameplates) do
-    local name = v.namePlateUnitToken
-    print(name)
+function RenderNamePlate(ParentFrame, PreviousFrame, Nameplate)
+    local frame = TNTG.Frames[Nameplate.namePlateUnitToken] or CreateNamePlateFrame(ParentFrame, PreviousFrame, Nameplate.namePlateUnitToken)
+
+    return frame
 end
 
 local MasterFrame = CreateFrame("Frame", nil, UIParent)
@@ -48,18 +71,19 @@ MasterFrameTexture.texture = MasterFrameTexture
 
 MasterFrame:SetPoint("TOPLEFT",0,0)
 MasterFrame:SetScript("OnUpdate", function(self, elapsed)
-    TNTG.last = TNTG.last + elapsed
-    if TNTG.last > 10 then
-        TNTG.last = 0
+    TNTG.Last = TNTG.Last + elapsed
+    if TNTG.Last > 10 then
+        TNTG.Last = 0
         local count = 0
         local nameplates = C_NamePlate.GetNamePlates(false)
+        local previous = nil
+        TNTG.Nameplates = {}
         for i, v in pairs(nameplates) do
-            print("checking")
             UpdateNamePlate(v)
-            print("done")
+            previous = RenderNamePlate(MasterFrame, previous, v)
             count = count + 1
         end
-        print(count .. " nameplates")
     end
 end)
 MasterFrame:Show()
+
